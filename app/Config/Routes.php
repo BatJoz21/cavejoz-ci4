379ee6dab0f4a78dacfb4411cf3114d6e5be9346
Jozka Siregar <85859565+BatJoz21@users.noreply.github.com>
2026-07-29 11:15:21 +0700

    Authentication routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -8,9 +8,12 @@ $routes->get('/register', 'Auths::openRegisterPage');
 $routes->post('/register', 'Auths::registerNewUser');
 $routes->get('/login', 'Auths::openLoginPage');
 $routes->post('/login', 'Auths::userLogin');
-$routes->post('/logout', 'Auths::userLogout');
 $routes->get('session-test', function () {
     dd(session()->get());
 });
 
-$routes->get('/search', 'Searches::search');
+$routes->group('/', ['filter' => 'jwtauth'], function($routes) {
+    $routes->post('/logout', 'Auths::userLogout');
+    
+    $routes->get('search', 'Searches::search');
+});
